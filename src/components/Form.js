0b79e362e3233aaf65d5d5a5184090bfc4710bd8
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-06-24 10:40:47 -0400

    updated form

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,23 +12,18 @@ const Form = ({ fetchJson }) => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(({
         "name": e.target.planetName.value,
-        "discoveredBy": e.target.discoveredBy.value,
-        "discoveredDate": e.target.discoveryDate.value,
         "temperature": e.target.temperature.value,
-        "bodyType": e.target.bodyType.value,
         "gravity": e.target.gravity.value,
-        "density": e.target.density.value,
-        "dimension": e.target.dimension.value,
       }))
     })
       .then(() => {
 
         console.log('Planets Added')
-        
+        history.push("/home")
         fetchJson()
       })
       
-      history.push("/home")
+      
   };
 
 
@@ -38,20 +33,8 @@ const Form = ({ fetchJson }) => {
 
       <form onSubmit={handleClick}>
         <input type="text" name="planetName" placeholder="Planet Name" />
-        <label>  </label>
-        <input type="text" name="discoveredBy" placeholder="Discovered By:" />
-        <label>  </label>
-        <input type="text" name="discoveryDate" placeholder="Discovery Date:" />
-        <label>  </label>
-        <input type="text" name="temperature" placeholder="Avg Temp. Kevin:" />
-        <label>  </label>
-        <input type="text" name="bodyType" placeholder="BodyType:" />
-        <label>  </label>
+        <input type="text" name="temperature" placeholder="Avg Temp. Kelvin:" />
         <input type="text" name="gravity" placeholder="Gravity:" />
-        <label>  </label>
-        <input type="text" name="density" placeholder="Density:" />
-        <label>  </label>
-        <input type="text" name="dimension" placeholder="Dimension:" />
         <hr></hr>
         <input
           type="submit"
--- src/components/PlanetDetails.js
+++ src/components/PlanetDetails.js
@@ -5,13 +5,8 @@ const PlanetDetails = ({ planet }) => {
         <div className='planet-details'>
 
             <h3 className={planet.name}> {planet.name}</h3>
-            <p> Discovered By: {planet.discoveredBy}</p>
-            <p> Discovery Date: {planet.discoveredDate}</p>
-            <p> Avg Temperature Kevin: {planet.temperature}</p>
-            <p> BodyType: {planet.bodyType}</p>
+            <p> Avg Temperature Kelvin: {planet.temperature}</p>
             <p> Gravity: {planet.gravity}</p>
-            <p> Density: {planet.density}</p>
-            <p> Dimension: {planet.dimension}</p>
 
         </div>
     )
